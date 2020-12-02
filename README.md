d078208764cf61e06e49be0daa012cbf3eee3780
nm0i <nm0i@me0w.net>
2020-12-02 03:28:12 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,15 @@
+Tisc - clear your qutebrowser cookies
+=====================================
+
+The script is very silly, it filters qutebrowser cookies by whitelist.
+
+Requirements
+------------
+
+Tcl, sqlite
+
+How to use
+----------
+
+Put your hosts into allow.txt, run the script. You may run it before
+qutebrowser starts, for example. Change cookiePath variable if needed.
---
+++ tisc.tcl
@@ -0,0 +1,25 @@
+#!/usr/bin/tclsh
+
+set cookiePath "~/.local/share/qutebrowser/webengine/Cookies"
+
+package require sqlite3
+
+sqlite db $cookiePath
+
+if {[catch {
+    set allowListFile [open allow.txt r]}
+    ]} {
+    puts "Could not open allow.txt."
+    exit
+}
+set allowList [read $allowListFile]
+close $allowListFile
+
+set allowRegexp [string map {\n | . \\.} $allowList]
+set allowRegexp [regsub -all {\|+$} $allowRegexp {}]
+
+puts $allowRegexp
+
+db eval {DELETE FROM cookies WHERE host_key NOT REGEXP :allowRegexp}
+db close
+
